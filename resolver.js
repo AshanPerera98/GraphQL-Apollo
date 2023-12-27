dc6195f84f74ace58625c33dd2ad8f7893e72403
Ashan Perera <ashantiwankaperera@gmail.com>
2023-12-27 13:20:59 +0530

    related data added

--- resolver.js
+++ resolver.js
@@ -21,4 +21,25 @@ export const resolvers = {
       return db.authors.find((author) => author.id === args.id);
     },
   },
+
+  Game: {
+    reviews(parent, args, context) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+
+  Author: {
+    reviews(parent, args, context) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+
+  Review: {
+    author(parent, args, context) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent, args, context) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+  },
 };
